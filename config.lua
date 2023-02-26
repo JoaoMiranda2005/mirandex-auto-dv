3d9c09d02b18334ff4b90472a57823a8c6deba17
João Miranda <jprmiranda678@gmail.com>
2023-02-26 14:35:08 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,14 +1,10 @@
 Config = {}
 
 Config.deleteTimer = 30 -- Tempo em Minutos / Time in Minutes
-
-Config = {
-    commandName = "delallveh",
+Config.commandName = "delallveh",
     -- Nome do comando que vai ser executado
     -- Se o nome do comando for mudado tem de ser mudado na ace_perm do server.cfg (explicado em baixo)
-
-    restrictCommand = true
+Config.restrictCommand = true
     -- Meter este valor como false vai fazer com que todos os players consigam usar o comando.
     -- Se meter este valor como true tens de adicionar uma ace perm para as pessoas com o grupo de admin usarem
     -- Exemplo: add_ace superadmin commmand.delallveh allow
-}
