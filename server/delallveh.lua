be644ec6a1cfe40b64b16da49e8fcae9731cfb8f
João Miranda <jprmiranda678@gmail.com>
2023-09-02 00:55:56 +0100

    Update delallveh.lua
    removida a treta da ace perm, agora quem tem o grupo admin consegue usar

--- server/delallveh.lua
+++ server/delallveh.lua
@@ -1,4 +1,12 @@
-RegisterCommand(Config.commandName, function(source, args, rawCommand)
-    Citizen.Wait(5000)
-    TriggerClientEvent("mirandex:delallveh", -1)
-end, Config.restrictCommand)
+ESX = exports["es_extended"]:getSharedObject()
+
+ESX.RegisterCommand('delallveh', function(xPlayer)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if source.getGroup() == 'admin' then
+        TriggerClientEvent("mirandex:delallveh", -1)
+    else
+        local msg = 'Não tens permissões para executar este comando!'
+        local type = error
+        local time = 5000
+        ESX.ShowNotification(msg, time, type)
+end)
